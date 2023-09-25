4246fdd71283a93f13341120e8331715aa6f196a
thegreatyamori <tod.jerson.morocho@gmail.com>
2023-09-25 00:52:47 -0500

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,18 +2,19 @@
 
 # This script accepts a parameter and prints it
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     echo "Usage: $0 set-version (v0.0.0)"
     exit 1
 fi
 
 version=$1
 message=$2
+path=$(pwd)
 
-cd app
-npm version $version --no-git-tag-version
-cd ..
+npm version $version --no-git-tag-version --prefix $path
 
+git add .
+git commit -m "release tag $version"
 git tag $version -m $message
 git push origin $version
 echo "New version available: $version"
