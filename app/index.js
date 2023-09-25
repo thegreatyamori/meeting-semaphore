d665cea5d2b9b8c7c62ba8d2a87971a5cffd6c71
thegreatyamori <tod.jerson.morocho@gmail.com>
2023-09-25 00:43:34 -0500

    add polling time as argv

--- app/index.js
+++ app/index.js
@@ -4,5 +4,4 @@ import { subscribe } from "./win_api.js";
 const argv = minimist(process.argv.slice(2));
 console.log(argv);
 
-const pollingTime = 5000;
-subscribe(pollingTime);
+subscribe(argv.delay);
